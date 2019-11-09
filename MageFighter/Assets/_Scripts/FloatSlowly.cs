7e96dd5f147f983f9e917deb67854625436548ad
Konrad Gumny <30446684+SadOnion@users.noreply.github.com>
2019-11-10 00:01:22 +0100

    Floating

---
+++ MageFighter/Assets/_Scripts/FloatSlowly.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatSlowly : MonoBehaviour
+{
+    public float degreesPerSecond = 15.0f;
+    public float amplitude = 0.5f;
+    public float frequency = 1f;
+
+
+    Vector3 posOffset = new Vector3();
+    Vector3 tempPos = new Vector3();
+
+
+    void Start()
+    {
+        posOffset = transform.position;
+    }
+
+
+    void Update()
+    {
+        tempPos = posOffset;
+        tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
+
+        transform.position = tempPos;
+    }
+
+}
--- MageFighter/Assets/_Scripts/InputHandler.cs
+++ MageFighter/Assets/_Scripts/InputHandler.cs
@@ -48,7 +48,7 @@ public class InputHandler : MonoBehaviour
                 { 
                     mage.CastSpell();
                 }
-            }`
+            }
         }
     }
 }
