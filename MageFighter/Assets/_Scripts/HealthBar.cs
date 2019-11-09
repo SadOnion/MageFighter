2172f60aa80843cd1dfe0f5ad48cc014fb73f15d
Konrad Gumny <30446684+SadOnion@users.noreply.github.com>
2019-11-09 12:37:40 +0100

    Before New Resource System

---
+++ MageFighter/Assets/_Scripts/HealthBar.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Image bar;
+    public Mage mage;
+
+    public void UpdateStat()
+    {
+        bar.fillAmount = mage.GetHpRatio();
+    }
+}
---
+++ MageFighter/Assets/_Scripts/Resource.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[Serializable]
+public class Resource
+{
+    public int currentValue;
+    public ResourceName name;
+
+    public UnityEvent OnResourceChange;
+
+    private int maxValue;
+}
+public enum ResourceName
+{
+    Health,
+    Mana
+}
